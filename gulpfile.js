d372259bf999cbfb74e59594b0d3a9c453c054fe
oddwang37 <78200508+oddwang37@users.noreply.github.com>
2021-09-07 22:15:27 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "../uber"
+        }
+    });
+
+    gulp.watch("../uber/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("styles"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
